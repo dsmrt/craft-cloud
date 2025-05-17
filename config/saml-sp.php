2f05f62b8e066bdf411c20c5e712d64a071dfb29
Damien Smrt <me@dsmrt.com>
2025-05-17 10:19:52 -0600

    entity id

--- config/saml-sp.php
+++ config/saml-sp.php
@@ -4,5 +4,5 @@ use Craft;
 use craft\helpers\App;
 
 return [
-    'entityId' => App::env('https://saml-sp-test-prod-c77f1e68.preview.craft.cloud')
+    'entityId' => App::env('https://saml-sp-test-prod-c77f1e68.preview.craft.cloud/')
 ];
